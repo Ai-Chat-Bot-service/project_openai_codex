1bc90c41c808d4746dfdf0cd3e9f2b3991d28ef2
daz2023 <125355767+daz2023@users.noreply.github.com>
2023-04-04 10:40:30 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const dotenv = require('dotenv')
-const cors = require('cors')
-const { Configuration, OpenAIApi } = require('openai')
-const fs = require('fs');
-const { PDFParser } = require('pdfreader');
+import { PDFParser } from 'pdfreader';
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import { Configuration, OpenAIApi } from 'openai';
+import fs from 'fs';
 
-dotenv.config()
+dotenv.config();
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -13,15 +13,15 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
-const app = express()
-app.use(cors())
-app.use(express.json())
+const app = express();
+app.use(cors());
+app.use(express.json());
 
 app.get('/', async (req, res) => {
   res.status(200).send({
     message: 'Hello from CodeX!'
-  })
-})
+  });
+});
 
 app.post('/', async (req, res) => {
   try {
@@ -54,9 +54,9 @@ app.post('/', async (req, res) => {
     parser.parseBuffer(fs.readFileSync(filePath));
 
   } catch (error) {
-    console.error(error)
+    console.error(error);
     res.status(500).send(error || 'Something went wrong');
   }
-})
+});
 
 app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
