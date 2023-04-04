9e5280c5b0caab559e46f2fc2a6d96ca8b0be224
daz2023 <125355767+daz2023@users.noreply.github.com>
2023-04-04 21:19:38 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,11 +1,9 @@
-import pdfreader from 'pdfreader';
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import { Configuration, OpenAIApi } from 'openai';
-import fs from 'fs';
+import express from 'express'
+import * as dotenv from 'dotenv'
+import cors from 'cors'
+import { Configuration, OpenAIApi } from 'openai'
 
-dotenv.config();
+dotenv.config()
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -13,61 +11,38 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
-const app = express();
-app.use(cors());
-app.use(express.json());
-
-let pdfText = '';
-const filePath = `${__dirname}/example.pdf`;
-
-// Extract text content from the PDF file
-const pdfParser = new pdfreader.PdfReader();
-pdfParser.parseFileItems(filePath, function (err, item) {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  else if (!item) {
-    console.log('PDF parsing complete');
-  }
-  else if (item.text) {
-    pdfText += item.text;
-  }
-});
+const app = express()
+app.use(cors())
+app.use(express.json())
 
 app.get('/', async (req, res) => {
   res.status(200).send({
     message: 'Hello from CodeX!'
-  });
-});
+  })
+})
 
 app.post('/', async (req, res) => {
   try {
     const prompt = req.body.prompt;
 
-    // Use the extracted text and user input prompt as the prompt in your OpenAI API request
-    openai.createCompletion({
+    const response = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: `${pdfText} ${prompt}`,
-      temperature: 0,
-      max_tokens: 3000,
-      top_p: 1,
-      frequency_penalty: 0.5,
-      presence_penalty: 0,
-    })
-      .then(response => {
-        res.status(200).send({
-          bot: response.data.choices[0].text
-        });
-      })
-      .catch(error => {
-        console.error(error);
-        res.status(500).send('Something went wrong');
-      });
+      prompt: `${prompt}`,
+      temperature: 0, // Higher values means the model will take more risks.
+      max_tokens: 200, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
+      top_p: 1, // alternative to sampling with temperature, called nucleus sampling
+      frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
+      presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+    });
+
+    res.status(200).send({
+      bot: response.data.choices[0].text
+    });
+
   } catch (error) {
-    console.error(error);
-    res.status(500).send('Something went wrong');
+    console.error(error)
+    res.status(500).send(error || 'Something went wrong, try again shortly');
   }
-});
+})
 
 app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
