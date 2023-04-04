f613d5b33d4ff097d1bbfdfd08939992cbb13ed6
daz2023 <125355767+daz2023@users.noreply.github.com>
2023-04-04 22:28:21 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import { Configuration, OpenAIApi } from 'openai';
-import fs from 'fs';
 import path from 'path';
 
 dotenv.config();
@@ -18,34 +17,74 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-let pdfText = '';
-const filePath = new URL('example1.pdf', import.meta.url).pathname;
+const pdfText = {
+  example1: '',
+  example2: '',
+};
 
-// Extract text content from the PDF file
-new pdfreader.PdfReader().parseFileItems(filePath, function (err, item) {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  else if (!item) {
-    app.listen(5000, () => console.log('AI server started on http://localhost:5000'));
-  }
-  else if (item.text) {
-    pdfText += item.text;
-  }
+const fileNames = Object.keys(pdfText);
+
+const filePaths = fileNames.reduce((result, fileName) => {
+  result[fileName] = path.join(__dirname, `${fileName}.pdf`);
+  return result;
+}, {});
+
+const loadPdf = (fileName) => {
+  const filePath = filePaths[fileName];
+  const chunks = [];
+  let result = '';
+
+  return new Promise((resolve, reject) => {
+    new pdfreader.PdfReader().parseFileItems(filePath, function (err, item) {
+      if (err) {
+        reject(err);
+        return;
+      }
+      else if (!item) {
+        result = chunks.join('');
+        resolve(result);
+      }
+      else if (item.text) {
+        chunks.push(item.text);
+      }
+    });
+  });
+}
+
+Promise.all(fileNames.map(loadPdf)).then((pdfs) => {
+  pdfs.forEach((pdf, index) => {
+    pdfText[fileNames[index]] = pdf;
+  });
+  app.listen(5000, () => console.log('AI server started on http://localhost:5000'));
+}).catch((error) => {
+  console.error(error);
 });
 
-let isFirstPrompt = true;
+app.get('/', async (req, res) => {
+  res.status(200).send({
+    message: 'Hello from CodeX!'
+  });
+});
 
 app.post('/', async (req, res) => {
   try {
-    const prompt = isFirstPrompt ? `${pdfText} ${req.body.prompt}` : req.body.prompt;
-    isFirstPrompt = false;
+    const prompt = req.body.prompt;
+    const isFirstPrompt = !req.body.context || req.body.context.length === 0;
+
+    let fullPrompt = '';
+
+    if (isFirstPrompt) {
+      for (const fileName of fileNames) {
+        fullPrompt += `${pdfText[fileName]} `;
+      }
+    }
+
+    fullPrompt += prompt;
 
     // Use the extracted text and user input prompt as the prompt in your OpenAI API request
     openai.createCompletion({
       model: "text-davinci-003",
-      prompt: `${prompt}`,
+      prompt: fullPrompt,
       temperature: 0,
       max_tokens: 3000,
       top_p: 1,
@@ -54,7 +93,8 @@ app.post('/', async (req, res) => {
     })
       .then(response => {
         res.status(200).send({
-          bot: response.data.choices[0].text
+          bot: response.data.choices[0].text,
+          context: [fullPrompt],
         });
       })
       .catch(error => {
