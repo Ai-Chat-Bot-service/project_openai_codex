1c8d39f243b4f290e70a76d9c360994a96cd1394
daz2023 <125355767+daz2023@users.noreply.github.com>
2023-04-10 22:46:21 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -48,11 +48,11 @@ app.post('/', async (req, res) => {
     openai.createCompletion({
       model: "text-davinci-003",
       prompt: `${prompt}`,
-      temperature: 0,
-      max_tokens: 3000,
+      temperature: 1,
+      max_tokens: 4000,
       top_p: 1,
       frequency_penalty: 0.5,
-      presence_penalty: 0,
+      presence_penalty: 1,
     })
       .then(response => {
         res.status(200).send({
