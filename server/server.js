10f936c6d082dccc57907ffc7a43078da0ce0685
daz2023 <125355767+daz2023@users.noreply.github.com>
2023-04-11 15:48:45 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ app.post('/', async (req, res) => {
 
     // Use the text and user input prompt as the prompt in your OpenAI API request
     openai.createCompletion({
-      model: "text-davinci-003",
+      model: "gpt-3.5-turbo",
       prompt: `${prompt}`,
       temperature: 0.5,
       max_tokens: 3000,
